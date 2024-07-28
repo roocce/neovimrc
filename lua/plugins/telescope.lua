a5528fb9a44052eef029b7009bdfac48c8be95b5
Rodolfo <ochoa.cetina@gmail.com>
2024-07-28 17:27:23 -0600

    Add telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        {'<leader>pf', require('telescope.builtin').find_files},
+        {'<leader>pg', require('telescope.builtin').live_grep},
+        {'<leader>pb', require('telescope.builtin').buffers},
+        {'<leader>ph', require('telescope.builtin').help_tags}
+    }
+}
