0e628750a594b6e65c67f9c76f38796021a5be91
Rodolfo <ochoa.cetina@gmail.com>
2024-07-28 16:09:33 -0600

    Enable rose-pine colorscheme

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -49,5 +49,8 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- colorscheme
+vim.cmd.colorscheme("rose-pine")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
