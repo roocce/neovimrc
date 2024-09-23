fb680894635ed2c4eb135c1a0de5fe571856c603
roocce <ochoa.cetina@gmail.com>
2024-09-23 02:08:27 -0600

    Add treesitter config function to autoinstall parsers
    Also listed some language parsers

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,34 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
+    build = ":TSUpdate",
+    opts = {
+        highlight = { enable = true },
+        ensure_installed = {
+            "bash",
+            "css",
+            "diff",
+            "html",
+            "javascript",
+            "jsdoc",
+            "json",
+            "lua",
+            "luadoc",
+            "markdown",
+            "markdown_inline",
+            "org",
+            "printf",
+            "python",
+            "regex",
+            "sql",
+            "toml",
+            "vim",
+            "vimdoc",
+            "vue",
+            "xml",
+            "yaml",
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
