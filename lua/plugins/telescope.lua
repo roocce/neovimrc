d2562e8d0d2573340ad46206ea459daa01d1285b
Rodolfo Ochoa Cetina <ochoa.cetina@gmail.com>
2025-09-30 05:05:59 -0600

    Add which-key config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,19 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-        {'<leader>pf', require('telescope.builtin').find_files},
-        {'<leader>pg', require('telescope.builtin').live_grep},
-        {'<leader>pb', require('telescope.builtin').buffers},
-        {'<leader>ph', require('telescope.builtin').help_tags}
-    },
+    dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-fzf-native.nvim" },
     init = function()
+        require('telescope').setup{}
         require('telescope').load_extension('fzf')
-    end
+        local wk = require('which-key')
+        wk.add({
+            {'<leader>p', group = 'Telescope'}
+        })
+    end,
+    keys = {
+        {'<leader>pf', '<cmd>Telescope find_files<cr>', desc = "find files"},
+        {'<leader>pg', '<cmd>Telescope live_grep<cr>', desc = "live grep"},
+        {'<leader>pb', '<cmd>Telescope buffers<cr>', desc = "buffers"},
+        {'<leader>ph', '<cmd>Telescope help_tags<cr>', desc = "help tags"}
+    },
 }
