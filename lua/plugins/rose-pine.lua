47f5ef620f775f16d0bb3ab88f3ebe92f3260536
Rodolfo Ochoa Cetina <ochoa.cetina@gmail.com>
2025-09-30 04:55:30 -0600

    Change theme styles

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,1 +1,16 @@
-return { "rose-pine/neovim", name = "rose-pine" }
+return {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+    opts = {
+        config = {
+            options = {
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = true,
+                },
+            }
+        }
+    }
+}
