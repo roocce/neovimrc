6951a536f2d38e45064e6bcdc5981a9f74d7f2de
Rodolfo Ochoa Cetina <ochoa.cetina@gmail.com>
2025-09-30 05:15:23 -0600

    Add orgmode return in insert mode

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -8,5 +8,20 @@ return {
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/refile.org',
     })
+
+    local wk = require('which-key')
+    wk.add({
+        {'<leader>o', group = 'Orgmode'}
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'org',
+        callback = function()
+            vim.keymap.set('i', '<S-CR>', '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
+                silent = true,
+                buffer = true,
+            })
+        end,
+    })
   end,
 }
